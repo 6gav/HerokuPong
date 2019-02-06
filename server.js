7039d2213be623cef98ec42fdea6a365f609a23b
gavset99@gmail.com <blackcat99>
2019-02-06 12:22:59 -0500

    Original Basic Router

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/getResponse', (req, res) => {
+  res.send({reponse: 'response'});
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+app.listen(port);
+console.log('Server hosted on port ' + port);
