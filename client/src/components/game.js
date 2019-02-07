f4a1d8a06000037b1a6f020089fcfe92a9330219
gavset99@gmail.com <blackcat99>
2019-02-07 10:57:16 -0500

    Added ball communication

--- client/src/components/game.js
+++ client/src/components/game.js
@@ -1,16 +1,76 @@
 import React, { Component } from 'react';
+import './game.css';
+
+var App;
+var ball;
 
 class Game extends Component{
+
+  constructor(props){
+    super(props);
+    this.keyDownFunction = this.keyDownFunction.bind(this);
+    this.keyUpFunction = this.keyUpFunction.bind(this);
+  }
+
+  keyDownFunction(event){
+    console.log('Key pressed ' + event.keyCode);
+  }
+
+  keyUpFunction(event){
+    console.log('Key released ' + event.keyCode);
+  }
+
+  componentDidMount(){
+    document.addEventListener("keydown", this.keyDownFunction, false);
+    document.addEventListener("keyup", this.keyUpFunction, false);
+    ball = document.getElementById('ball');
+    this.testServer();
+  }
+
+  async testServer(){
+    while(true){
+
+      const response = await fetch('/api/getCount', {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const body = await response.json();
+      if(body){
+
+        this.setPosition(body);
+
+      }
+    }
+  }
+
+  componentWillUnmount(){
+    document.removeEventListener("keydown", this.keyDownFunction, false);
+    document.removeEventListener("keyup", this.keyUpFunction, false);
+  }
+
+  setPosition(body){
+    var x = body.ball.xpos * (window.innerWidth - 200) + "px";
+    var y = body.ball.ypos * (window.innerHeight - 200)+ "px";
+    ball.style.left = x;
+    ball.style.top = y;
+
+  }
 
   render(){
     return(
       <div className = 'App'>
-        <header className = 'App-header'>
+        <header className = 'App-header' onKeyPress={this.handleKey}>
+          <img src="https://www.pngarts.com/files/1/Meatball-PNG-Image-With-Transparent-Background.png" id='ball'/>
           <p>Game page</p>
         </header>
       </div>
     );
   }
+}
+
+async function sleep(millis){
+  return new Promise(resolve => setTimeout(resolve, millis));
 }
 
 export default Game;
--- server.js
+++ server.js
@@ -1,18 +1,76 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+//Game Variables
+var ball = {
+  xpos: 0,
+  ypos: 0,
+  xvel: 0.002,
+  yvel: 0.005,
+}
+
+
+
+
+game();
+
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/getResponse', (req, res) => {
   res.send({reponse: 'response'});
 });
+
+var count = 0;
+
+app.get('/api/getCount', (req, res) => {
+  console.log('Got request with ');
+  console.log(req.body);
+  res.send({ball: ball});
+});
+
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 app.listen(port);
+
 console.log('Server hosted on port ' + port);
+
+
+async function game(){
+  setTimeout(gameTick, 50);
+
+
+}
+
+function gameTick(){
+  console.log('tick');
+
+  ball.xpos += ball.xvel;
+  ball.ypos += ball.yvel;
+  if(ball.xpos > 1){
+    ball.xpos = 1;
+    ball.xvel = -ball.xvel + (Math.random() / 100);
+  }
+  if(ball.ypos > 1){
+    ball.ypos = 1;
+    ball.yvel = -ball.yvel + (Math.random() / 100);
+  }
+  if(ball.xpos < 0){
+    ball.xpos = 0;
+    ball.xvel = -ball.xvel;
+  }
+  if(ball.ypos < 0){
+    ball.ypos = 0;
+    ball.yvel = -ball.yvel;
+  }
+  setTimeout(gameTick, 10);
+}
